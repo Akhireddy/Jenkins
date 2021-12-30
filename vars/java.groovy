12bcac6ec4997bebaa26f495aac6cbe09b83443f
Akhireddy <33176548+Akhireddy@users.noreply.github.com>
2021-12-30 22:09:27 +0800

    Create java.groovy

---
+++ vars/java.groovy
@@ -0,0 +1,36 @@
+def call() {
+  pipeline {
+    agent {
+      label "${BUILD_LABEL}"
+    }
+
+    triggers {
+      pollSCM('*/2 * * * *')
+    }
+
+    stage('Compile the Code') {
+      steps {
+        sh 'echo compile code'
+      }
+
+      stage('Check the Code Quality') {
+        steps {
+          script { 
+            common.sonarQube()
+          }
+        }
+      }
+
+      stage('Lint Checks') {
+        steps {
+          sh 'echo Test Cases'
+        }
+      }
+
+      stage('Test Cases') {
+        steps {
+          sh 'echo Test Cases'
+        }
+      }
+
+    }
