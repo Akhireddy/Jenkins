391da3e71a30914f4282eb1c11bd723de680f1ac
Akhireddy <33176548+Akhireddy@users.noreply.github.com>
2022-01-04 17:39:56 +0800

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,5 +3,5 @@
 //}
 
 def sonarQube() {
-  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://34.125.133.79:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://34.125.104.3:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 }
