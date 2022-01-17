546957b490f0d08728e8749a5a3e4495c9ce3ee6
Akhireddy <akhir1989@gmail.com>
2022-01-17 17:21:59 +0800

    added security group details

---
+++ terraformspeed.groovy
@@ -0,0 +1,25 @@
+folder('Mutable') {
+    displayName('Mutable')
+    description('Mutable')
+}
+
+pipelineJob('Mutable/VPC') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/Akhireddy/terraform-vpc.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
