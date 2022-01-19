62d8d1ab8b68a1ab2ceb30030efdfac69452f2e3
Akhireddy <akhir1989@gmail.com>
2022-01-19 16:35:07 +0800

    var file updated with cidr

--- seedjob.groovy
+++ seedjob.groovy
@@ -145,3 +145,25 @@ pipelineJob('Mutable/VPC') {
     }
   }
 }
+
+
+pipelineJob('Mutable/ALB') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Akhireddy/Jenkins.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('jenkinsfile-Mutable-Infra')
+      'lightweight'(true)
+    }
+  }
+}
